db59ab7ecc8ea68b986b0768b415d56ae6570900
Matheus Silva <matheus090298@hotmail.com>
2022-11-29 22:49:13 -0300

    Aplicando refatoração do código

---
+++ js/screen.js
@@ -0,0 +1,47 @@
+const boxWordGame = document.querySelector('#box-button-new-wordGame');
+const boxGame = document.querySelector('#box-game');
+const boxLetter = document.querySelector('#box-letter');
+const boxKeyboard = document.querySelector('#box-keyboard');
+
+const buttonNewGame = document.querySelector('#button-new-game');
+buttonNewGame.addEventListener('click' , function(){
+
+    boxWordGame.style.display = 'none';
+    boxGame.style.display = 'flex';
+    boxLetter.style.display = 'flex';
+    boxKeyboard.style.display = 'flex';
+})
+
+const boxWordTip = document.querySelector('#box-new-wordTip');
+const boxSubmit = document.querySelector('#box-button-submit')
+
+const buttonNewWord = document.querySelector('#button-new-word');
+buttonNewWord.addEventListener('click' , function(){
+
+    boxWordGame.style.display = 'none';
+    boxWordTip.style.display = 'flex';
+    boxSubmit.style.display = 'flex';
+
+})
+
+const buttonSubmit = document.querySelector('#button-submit');
+buttonSubmit.addEventListener('click' , function(){
+
+    boxWordGame.style.display = 'flex';
+    boxWordTip.style.display = 'none';
+    boxSubmit.style.display = 'none';
+})
+
+const home = document.querySelector('#home');
+home.addEventListener('click' , function(){
+
+    boxWordGame.style.display = 'flex';
+    boxGame.style.display = 'none';
+    boxLetter.style.display = 'none';
+    boxKeyboard.style.display = 'none';
+    boxWordTip.style.display = 'none';
+    boxSubmit.style.display = 'none';
+
+    document.querySelector('.play-game').id = 'play-game';
+})
+
--- js/script.js
+++ js/script.js
@@ -1,135 +1,73 @@
+let gameWord = ['lojaz' , 'carroy' , 'escola'];
+let dica = ['estabelecimento' , 'transporte' , 'estudos'];
 
-let palavras = ['limonada', 'passarinho', 'serenata', 'faculdade', 'campeonato', 'furadeira', 'restaurante', 'caminhonete', 'violino', 'engenheiro' , 'geografia', 'passaporte'];
-var dicas = ['Bebida', 'Animal', 'Apresentação', 'Estudos', 'Disputa', 'Ferramenta', 'Comida', 'Transporte', 'Som' , 'Profissão', 'Disciplina', 'Documento']
-let palavraAleatoria = sorteiaPalavra();
-var letras = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'w', 'y', 'z'];
-var separacao = palavras[palavraAleatoria].split(''); //Array com as letras da palavra
+function newGame(drawNumber){
 
-var canvas = document.getElementById('jogo');
-var pincel = canvas.getContext('2d');
-var corCorpo = 'black';
-var corTraco = 'darkblue';
+    const boxLetter = document.querySelector('#box-letter');
+    
+    for(var i = 0; i < gameWord[drawNumber].length; i++){
+    
+    const newLetter = document.createElement('p');
+    const newTextLetter = document.createTextNode(gameWord[drawNumber][i]);
 
-var inputs = selecionar('.inputs');
-var novaPalavra = selecionar('.nova-palavra');
-var novaDica = selecionar('.nova-dica');
-var adicionarNovaPalavra = selecionar('.adicionar-nova-palavra');
-var iniciarJogo = selecionar('.iniciar-jogo');
-var enviarPalavra = selecionar('.enviar-palavra');
-var game = selecionar('.canvas-game');
-var canvas = selecionar('.canvas');
-var recarregar = selecionar('.muda-palavra');
-var botao = selecionar('.botao');
-var rede = selecionar('.rede');
-var funciona = true;
-var teclado = selecionar('.teclado');
-var divEnviar = selecionar('.bnt-enviar');
-var divInicio = selecionar('.bnt-inicio');
-
-inputs.addEventListener('keypress', function(e){
-
-    if(!verificaChar(e)) {
-        e.preventDefault();
+    newLetter.classList.add('hidden-letter')
+   newLetter.appendChild(newTextLetter) /**/
+    boxLetter.appendChild(newLetter)
     }
+}
+
+function drawNumber(){
+    return Math.floor(Math.random()*(gameWord.length)) 
+}
+
+let randomNumber = drawNumber();
+
+newGame(randomNumber);/**/
+
+const reload = document.querySelector('#reload');
+
+reload.addEventListener('click' , function(){
+
+    randomNumber = drawNumber();
+    const boxLetter = document.querySelector('#box-letter');
+    const keys = document.querySelector('.letter');
+    
+    keys.disabled = false;
+    boxLetter.innerHTML = '';
+    newGame(randomNumber);
 })
 
-function verificaChar(e) {
+let errorCount = 0;
 
-    const char = String.fromCharCode(e.keyCode);
-    
-    const padrão = '[a-z]';
+function teste(letter){
 
-    if(char.match(padrão)){
-        console.log(char);
-        return true; 
-    }
-}
+    let correct = false;
+    const newWord = gameWord[randomNumber];
+    const key = document.getElementById(letter);
 
-function sorteiaPalavra(){
-    return Math.round(Math.random()*(palavras.length-1))
-}
-
-iniciarJogo.addEventListener('click', function iniciarjogo(){
-    
-    desenhaJogo();
-    desenhaTraco(20, 60, palavraAleatoria);
-    telaDoJogo();
-
-    document.addEventListener('keydown', function verificaLetraTeclada(event){
- 
-        for(var i = 0; i < 26; i++){
-            
-            if(event.key == letras[i]){
-            verifica(event.key);
-            letras[i] = ""; 
-            }
+    for(var i = 0; i <= newWord.length; i++){
+        if(letter == newWord[i]){
+            console.log(newWord[i] + letter)
+            key.disabled = true;
+            correct = true;
         }
-                        
-    })
-})
+    }
 
-
-enviarPalavra.addEventListener('click' , function(){
-    palavras.push(novaPalavra.value);
-    dicas.push(novaDica.value);
-    console.log(palavras);
-    console.log(dicas);
-    novaDica.value = "";
-    novaPalavra.value = ""; 
-    inicio();
-})  
-
-adicionarNovaPalavra.onclick = function(){
-    telaNovaPalavra();
-}
-
-function reinicia(){
-    pincel = canvas.getContext('2d');
-    pincel.clearRect(0, 0, 800, 600)
-    inicio();
-}
-
-function somDaDerrota(){
-    const newLocal = 'audios/derrota.wav';
-    let sound1 = new Audio(newLocal);
-    sound1.play();
-}
-
-function somDaVitoria(){
-    let sound2 = new Audio('audios/vitoria.wav');
-    sound2.play();
-}
-
-function selecionar(identificador){
-    return document.querySelector(identificador);   
-}
-
-
-
-/* var letra = document.querySelectorAll(".letras");
-
-letra.forEach(function(tecla){
-    tecla.addEventListener("click", function(){
-        var teclaAtual = tecla.value
-        console.log(teclaAtual);
-
-        var i = 0;
-        if(teclaAtual == letras[i]){
+    if(correct == false){
         
-            verifica(letras[i]);
-            letras[i] = ""; 
-            i++;
-        }  
-    })
-    })
-    
-    
-       for(var i = 1; i <= 26; i++){
-            if(te)
-            tecla[i].addEventListener('click', function(e){
-            console.log(tecla[i].value);
-            })
-        }   */
+        key.disabled = true;
+        errorCount = errorCount + 1;
+        
+        if(errorCount == 6){
 
+            document.querySelector('#box-letter').style.display = 'none';
+            document.querySelector('#box-keyboard').style.display = 'none';
+            
+            document.querySelector('.play-game').id = 'game-over';
+        } else{
+            document.querySelector('.play-game').id = 'bg'+errorCount;
+        }
+          
+    }
 
-
+}
